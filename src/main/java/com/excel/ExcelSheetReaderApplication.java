daefb332084e70d369c0020ac994ecd126a7c5a1
unknown <arnavmalhotra338@gmail.com>
2022-01-01 22:46:06 +0530

    Adding spring boot files

---
+++ src/main/java/com/excel/ExcelSheetReaderApplication.java
@@ -0,0 +1,13 @@
+package com.excel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelSheetReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelSheetReaderApplication.class, args);
+	}
+
+}
